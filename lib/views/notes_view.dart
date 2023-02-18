54ab1774a6e13d83b24a8c8aed53da84d5c8a5d0
Mohamed Fouad <m.elsayedfouad99@gmail.com>
2023-02-18 21:55:01 +0200

    finish_design_notes_view

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:notes_app_with_hive/widgets/notes_view_body.dart';
+import '../widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: NotesViewBody());
+    return Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: const Icon(Icons.add),
+        ),
+        body: const NotesViewBody());
   }
 }
