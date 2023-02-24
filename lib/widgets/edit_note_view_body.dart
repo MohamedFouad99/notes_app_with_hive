9b406609576e1d557067960bd024e55381241b4b
Mohamed Fouad <m.elsayedfouad99@gmail.com>
2023-02-24 17:38:17 +0200

    update_ui_in_edit_view

--- lib/widgets/edit_note_view_body.dart
+++ lib/widgets/edit_note_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app_with_hive/widgets/custom_app_bar.dart';
+import 'package:notes_app_with_hive/widgets/custom_text_field.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({super.key});
@@ -17,6 +18,16 @@ class EditNoteViewBody extends StatelessWidget {
           ),
           const SizedBox(
             height: 12,
+          ),
+          const CustomTextFiled(
+            hint: 'Title',
+          ),
+          const SizedBox(
+            height: 18,
+          ),
+          const CustomTextFiled(
+            hint: 'Content',
+            maxLines: 6,
           ),
         ],
       ),
